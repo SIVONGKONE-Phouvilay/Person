deb81832c18c2383f75d6c4a71de850ff1e439c2
SIVONGKONE-Phouvilay <Asphou19@gmail.com>
2022-10-04 22:17:57 +0700

    Bai IEmployee

--- src/IEmployee/Entry.java
+++ src/IEmployee/Entry.java
@@ -1,11 +1,11 @@
 package IEmployee;
 public class Entry {
     public static void main(String[] args) {
-        IEmployee employee1 = new PartTimeEmployee("Trung", 45000, 5);
+        IEmployee employee1 = new PartTimeEmployee("Adexa KEOBORLAKOT", 45000, 5);
         System.out.println("Name: " + employee1.getName());
         System.out.println("Salary per day: " + employee1.calculateSalary());
 
-        IEmployee employee2 = new FullTimeEmployee("Linh", 65000);
+        IEmployee employee2 = new FullTimeEmployee("Phonekham VILAYVONG", 65000);
         System.out.println("Name: " + employee2.getName());
         System.out.println("Salary per day: " + employee2.calculateSalary());
     }
